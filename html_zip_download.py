2f211a4df62d8745cd84e463179b04e3bbd51d55
jweber <joel.weber97@gmail.com>
2022-01-01 14:49:18 -0500

    starting top image zip download

--- html_zip_download.py
+++ html_zip_download.py
@@ -4,7 +4,7 @@ import os
 
 #works for the top_2016 html file and associated jp2 imagery
 
-with open('top_2016.html', 'r') as f:
+with open('./top_2016.html', 'r') as f:
     contents = f.read()
     soup = BeautifulSoup(contents, 'html.parser')
 
@@ -20,10 +20,9 @@ with open('top_2016.html', 'r') as f:
             links2.append(i)
     print(len(links2))
 
-    links3 = links2[:2]
 
-    output_dir = './full_images'
-    for i in links3:
+    output_dir = r'G:/top_images'
+    for i in links2:
         print(i)
         name = i.split('/')[-1]
         print(name)
