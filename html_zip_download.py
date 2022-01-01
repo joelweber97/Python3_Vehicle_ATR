42ee16c3ad77da1dd1ceca2947a93d93b2ac90fa
Joel Weber <joel.weber97@gmail.com>
2022-01-01 13:02:11 -0500

    completed test run of the zip downloader. I am able to download naip imagery that's in sid format. I want to get out the other files though so I can possibly get jp2 format instead. would save a bunch of time on the sid to tif convert

--- html_zip_download.py
+++ html_zip_download.py
@@ -1,11 +1,32 @@
 from bs4 import BeautifulSoup
+import wget
+import os
 
-with open('index.html', 'r') as f:
+with open('html.html', 'r') as f:
 
     contents = f.read()
 
     soup = BeautifulSoup(contents, 'html.parser')
 
-    
+    links = []
+    for a in soup.find_all('a', class_="ant-btn ant-btn-link", href=True):
+        links.append(a['href'])
 
- 
+    #print(links)
+    print('success')
+    links2 = []
+    for i in links:
+        if 'nc.zip' in i:
+            links2.append(i)
+    
+    links3 = links2[:1]
+    #print(links3)
+
+
+    output_dir = './full_images'
+    for i in links3:
+        #print(i)
+        name = i.split('/')[-1]
+        print(name)
+        print(os.path.join(output_dir, name))
+        wget.download(i, os.path.join(output_dir, name))
