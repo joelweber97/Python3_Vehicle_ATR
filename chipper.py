72c937b6d4a7db79486b090ba5e8752423125ab5
Joel Weber <joel.weber97@gmail.com>
2022-01-01 13:39:10 -0500

    finished html_zip_downloader and beginning the chipper python file

--- chipper.py
+++ chipper.py
@@ -2,4 +2,12 @@ import cv2
 import glob
 import numpy
 
+"""The goal of this script is to read in random images from a directory into cv2, allow the user to click
+in the middle of a vehicle, have cv2 create a bounding box of 22x22 pixels around that point, and extract
+that image chip into a new folder which will be used for training a cnn.
 
+top_2016 imagery chips should be 22x22 (or maybe 25x25) pixels to fit most personal vehicles. 
+
+top_2016 imagery has a spatial resolution of .5 meters
+
+"""
--- html_zip_download.py
+++ html_zip_download.py
@@ -2,10 +2,40 @@ from bs4 import BeautifulSoup
 import wget
 import os
 
-with open('html.html', 'r') as f:
+#works for the top_2016 html file and associated jp2 imagery
 
+with open('top_2016.html', 'r') as f:
     contents = f.read()
+    soup = BeautifulSoup(contents, 'html.parser')
 
+    links = []
+    for a in soup.find_all('a', class_="ant-btn ant-btn-link", href=True):
+        links.append(a['href'])
+
+    #print(links)
+    print('success')
+    links2 = []
+    for i in links:
+        if 'nc-cir.zip' in i:
+            links2.append(i)
+    print(len(links2))
+
+    links3 = links2[:2]
+
+    output_dir = './full_images'
+    for i in links3:
+        print(i)
+        name = i.split('/')[-1]
+        print(name)
+        print(os.path.join(output_dir, name))
+        wget.download(i, os.path.join(output_dir, name))
+
+
+'''
+#works for the naip_2020 html file and associated sid imagery
+
+with open('naip_2020.html', 'r') as f:
+    contents = f.read()
     soup = BeautifulSoup(contents, 'html.parser')
 
     links = []
@@ -18,15 +48,13 @@ with open('html.html', 'r') as f:
     for i in links:
         if 'nc.zip' in i:
             links2.append(i)
-    
-    links3 = links2[:1]
-    #print(links3)
-
 
     output_dir = './full_images'
-    for i in links3:
+    for i in links2:
         #print(i)
         name = i.split('/')[-1]
         print(name)
         print(os.path.join(output_dir, name))
         wget.download(i, os.path.join(output_dir, name))
+
+'''
