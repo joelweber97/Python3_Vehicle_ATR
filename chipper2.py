efe7def2d13f953b13ad3c7ed733c3d844c0f2b8
jweber <joel.weber97@gmail.com>
2022-01-04 17:21:32 -0500

    modified the vehicle locations shp and i think I figured out the tf,gpd,gdal incompatibility issues. also merged over to a notebook because vs code was throwing some bs fiona error that I dont want to solve

--- chipper2.py
+++ chipper2.py
@@ -7,10 +7,9 @@ pull the points that fall within that image, extract +- 6 meters in each directi
 to get the extents of the extract, and then extract the image, and then save it into a 
 new directory for use in the cnn.
 """
-'''
-#export OPENCV_IO_ENABLE_JASPER = true
-import os
-os.environ['OPENCV_IO_ENABLE_JASPER'] = 'true'
+
+
+import tensorflow as tf
 import geopandas as gpd
 
 import shapely
@@ -29,7 +28,7 @@ shp2['maxy'] = shp2['ycoord'] + 6
 
 shp2['box'] = shp2.apply(lambda x: box(x['minx'], x['miny'], x['maxx'], x['maxy']), axis = 1)
 print(shp2.head())
-'''
+
 import gdal
 
-gdal.Open('G:/top_images/top15-50cm_48023_nc-cir/top15-nc-cir-50cm-baylor_48023.jp2')
+ds = gdal.Open('G:/top_images/top15-50cm_48023_nc-cir/top15-nc-cir-50cm-baylor_48023.jp2')
